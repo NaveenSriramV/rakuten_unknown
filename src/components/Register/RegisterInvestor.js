a794940fd4b66bea48250a996c5f32d79f3e7f95
ani <amazinganirudhhere@gmail.com>
2021-04-10 03:35:40 +0530

    'otp'

--- src/components/Register/RegisterInvestor.js
+++ src/components/Register/RegisterInvestor.js
@@ -15,25 +15,11 @@ function RegisterInvestor() {
         
     }
     
-    const onSubmit=(e)=>{
+    const onSubmit=async(e)=>{
         e.preventDefault();
-        // console.log(FormDetails);
-        axios.post("http://localhost:8000/investorRegister",FormDetails)
-            .then((result)=>{
-                if (result.data==='success')
-                window.location='/login'
-                else {
-                    alert('Cannot add user')
-                }
-
-            }
-            );
-
-        
-    }
-    const handleclick=async()=>{
+        // console.log(FormDetails.phoneNo);
         let recaptcha = await new firebase.auth.RecaptchaVerifier('recaptcha');
-        let number = '+91 6382729213';
+        let number ="+91" + FormDetails.phoneNo;
         await firebase.auth().signInWithPhoneNumber(number,recaptcha).then((e)=>{
             let code = prompt('enter the otp','');
             if (code === null) console.log('error');
@@ -42,21 +28,52 @@ function RegisterInvestor() {
 
             })
         })
-        .catch(error=>{
-            console.error(error)
-        })
+        .then(()=>{
+            
+            // console.log(FormDetails);
+            axios.post("http://localhost:8000/investorRegister",FormDetails)
+            .then((result)=>{
+                if (result.data!='success')
+                alert('Cannot add user')
+                window.location='/login'
+            }
+            
+        
+        );
+    })
+    .catch(error=>{
+        console.error(error)
+    })
+        
+        
     }
+    // const handleclick=async()=>{
+    //     let recaptcha = await new firebase.auth.RecaptchaVerifier('recaptcha');
+    //     let number = '+91 6382729213';
+    //     await firebase.auth().signInWithPhoneNumber(number,recaptcha).then((e)=>{
+    //         let code = prompt('enter the otp','');
+    //         if (code === null) console.log('error');
+    //         e.confirm(code).then(result=>{
+    //             console.log(result)
+
+    //         })
+    //     })
+    //     .catch(error=>{
+    //         console.error(error)
+    //     })
+    // }
 
     return (
         <Grid className={classes.body} justify='center' container>
             <Grid container spacing={3} justify='center' className={classes.grid} >
-            <Button onClick={handleclick}>click</Button>
+            {/* <Button onClick={handleclick}>click</Button> */}
             <form onSubmit={onSubmit}>
             <Typography className={classes.header} variant='h5' gutterBottom>Register your company Here</Typography>
                 
                 <TextField name='name' onChange={onchangec} className={classes.textField} label='Name'  color="secondary"  fullWidth required /> 
                 <TextField name='phoneNo' onChange={onchangec} className={classes.textField} label='Phone Number'  color="secondary"  fullWidth required type='tel' /> 
                 <TextField name='password' onChange={onchangec} className={classes.textField} label='Password'  color="secondary" type='password' fullWidth required/> 
+                <div id="recaptcha"></div>                      
                 <Button  className={classes.button} color='secondary' type='submit' variant='contained'>Submit</Button>
             </form>
 
